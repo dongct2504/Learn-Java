b36cee8e4671e469be8686b44e3c7a6e5836b6cd
blackd000 <dongminhcho89@gmail.com>
2023-01-30 02:09:18 +0700

    add

--- happy_coding/mma/dong/ui/ProductManagerFrame.java
+++ happy_coding/mma/dong/ui/ProductManagerFrame.java
@@ -1,17 +1,26 @@
 package dong.ui;
 
-import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
+import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.ListSelectionModel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
+import dong.business.Product;
+import dong.db.DBException;
+import dong.db.ProductDB;
+
 @SuppressWarnings("serial")
 public class ProductManagerFrame extends JFrame {
+
+	private final Font FONT = new Font("Arial", Font.PLAIN, 14);
 
 	private ProductTableModel productTableModel;
 	private JTable productTable;
@@ -24,10 +33,13 @@ public class ProductManagerFrame extends JFrame {
 			System.out.println(e);
 		}
 		setTitle("Product Manager");
-		setSize(768, 384);
+		setSize(700, 600);
 		setLocationByPlatform(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+		add(buildButtonPanel(), BorderLayout.SOUTH);
+		productTable = buildProductTable();
+		add(new JScrollPane(productTable), BorderLayout.CENTER);
 		setVisible(true);
 	}
 
@@ -36,14 +48,17 @@ public class ProductManagerFrame extends JFrame {
 
 		JButton addButton = new JButton("Add");
 		addButton.addActionListener((event) -> doAddButton());
+		addButton.setFont(FONT);
 		panel.add(addButton);
 
 		JButton editButton = new JButton("Edit");
 		editButton.addActionListener((event) -> doEditButton());
+		editButton.setFont(FONT);
 		panel.add(editButton);
 
-		JButton deleteButton = new JButton("Edit");
+		JButton deleteButton = new JButton("Delete");
 		deleteButton.addActionListener((event) -> doDeleteButton());
+		deleteButton.setFont(FONT);
 		panel.add(deleteButton);
 
 		return panel;
@@ -62,10 +77,39 @@ public class ProductManagerFrame extends JFrame {
 
 	private void doDeleteButton() {
 		int selectedRow = productTable.getSelectedRow();
+		if (selectedRow == -1) {
+			JOptionPane.showMessageDialog(this,
+					"Vui lòng lựa chọn sản phẩm!!",
+					"Chưa chọn",
+					JOptionPane.ERROR_MESSAGE);
+		} else {
+			Product product = productTableModel.getProduct(selectedRow);
+			int ask = JOptionPane.showConfirmDialog(this,
+					"Bạn có chắc muốn xóa sản phẩm '" + 
+							product.getDescription() + "' ra khỏi database không?\n" + 
+							"Điều này sẽ không thể khôi phục!!",
+					"Chắc chưa",
+					JOptionPane.YES_NO_OPTION);
+			if (ask == JOptionPane.YES_OPTION) {
+				try {
+					ProductDB.delete(product);
+					productTableModel.databaseUpdated();
+				} catch (DBException e) {
+					System.out.println(e);
+				}
+			}
+		}
 	}
-	
 
-	
+	private JTable buildProductTable() {
+		productTableModel = new ProductTableModel();
+		JTable table = new JTable(productTableModel);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setBorder(null);
+		table.setRowHeight(20);
+		table.setFont(FONT);
+		return table;
+	}
 
 	public static void main(String[] args) {
 		new ProductManagerFrame();
